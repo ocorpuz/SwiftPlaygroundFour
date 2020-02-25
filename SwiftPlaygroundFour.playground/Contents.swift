e5594b7c20032f52d55843737c25b1b0c5a381c9
ocorpuz <59846122+ocorpuz@users.noreply.github.com>
2020-02-25 10:23:03 -1000

    SwiftPlaygroundFour + Comments Completed

--- SwiftPlaygroundFour.playground/Contents.swift
+++ SwiftPlaygroundFour.playground/Contents.swift
@@ -1,3 +1,40 @@
 import UIKit
 
 var str = "Hello, playground"
+
+//Part 1-3 Completed
+
+//Part 4:
+// set a range of numbers from 1-5. "..." range in code
+for number in 1...5
+{
+    //print range
+    print(number)
+}
+//initialize "sum" variable equal to 0
+var sum = 0
+//<constant> in <range>
+for number in 1...5
+{
+    //exexcute code to sum up numbers in range
+    sum+=number
+    print(sum)
+}
+
+//Part 5: Click black box on line 17 to display a graph
+
+//Part 6: While Loops
+//Initialize "i" as set variable and set it first number in range, 1
+var i = 1
+//Create conditional statement so that "i" can only go to 10, nothing more
+while i <= 10
+{
+//print the "i" variable
+    print(i)
+//add "+ 1" to "i" to run through the loop
+    i = i + 1
+}
+
+
+
+
